a00df59eb340950e0ffed377d8b91daae3df6391
Hamza <Liongaming386@gmail.com>
2026-04-16 15:21:53 +0200

    das

--- obj/Release/net10.0/balato.AssemblyInfo.cs
+++ obj/Release/net10.0/balato.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("balato")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8bc6ab18da9839cafc365b8f96b61154c7ccca")]
 [assembly: System.Reflection.AssemblyProductAttribute("balato")]
